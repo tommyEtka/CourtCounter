5efdbc1250bdfee94f96761d3610ed82a61fbb6b
tommyEtka <thurma13@gmail.com>
2018-04-05 19:09:49 -0400

    added links to buttons

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(0);
     }
+
+
+
+    /**
+
+     * Displays the given score for Team A.
+
+     */
+
+    public void displayForTeamA(int score) {
+
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+
+        scoreView.setText(String.valueOf(score));
+
+    }
+    public void onePoint(View view) {
+        points=points+1;
+        displayForTeamA(points);
+
+    }
+    public void twoPoints(View view) {
+        points=points+2;
+        displayForTeamA(points);
+
+    }
+    public void threePoints(View view) {
+        points=points+3;
+        displayForTeamA(points);
+
+    }
+
 }
